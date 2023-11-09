78296dc4d6c020e3e1bc3fd8e0fa8dd59144e90e
Austin Vichich <avichich4@gmail.com>
2023-11-08 18:45:08 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(array) {
+    const win = array.find(game => game.result === 'W');
+    if (win) {
+      return win.year;
+    }
+    return undefined;
+  }
+
+  superbowlWin(record);
